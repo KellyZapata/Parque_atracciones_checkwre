09453d11f080cbd53c990caecd2b4ab71fb1b174
Kelly Zapata <k.zapata1@utp.edu.co>
2022-10-30 21:35:08 -0500

    doble factor en login

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -2,31 +2,34 @@ const generador = require('password-generator');
 const bcrypt = require('bcrypt');
 const Usuario = require('../models/usuario');
 const jwt = require('jsonwebtoken');
+const sgMail = require('@sendgrid/mail')
+
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 module.exports = {
-    Login: (req, res) => {
+    Login: async (req, res) => {
+      try {
         let fetchedUser;
-        Usuario.findOne({correo_electronico: req.body.correo_electronico}).then(user=>{
-          if(!user){
-            return res.status(401).json({
-              token: "error",
-              expiresIn: "error",
-              role: "error",
-              message: "Correo electrónico invalido."
-            });
-          }
-          fetchedUser=user;
-          return bcrypt.compare(req.body.clave, user.clave);
-        })
-        .then(result =>{
-          if(!result){
-            return res.status(401).json({
-              token: "error",
-              expiresIn: "error",
-              role: "error",
-              message: "Contraseña invalida."
-            });
-          }
+        fetchedUser = await Usuario.findOne({correo_electronico: req.body.correo_electronico})
+        if(!fetchedUser){
+          return res.status(401).json({
+            token: "error",
+            expiresIn: "error",
+            role: "error",
+            message: "Correo electrónico invalido."
+          });
+        }
+        const encrypt = await bcrypt.compare(req.body.clave, fetchedUser.clave)
+        if(!encrypt){
+          return res.status(401).json({
+            token: "error",
+            expiresIn: "error",
+            role: "error",
+            message: "Contraseña invalida."
+          });
+        }
+        if (req.body.codigo && req.body.codigo == fetchedUser.codigo_autenticacion) {
           const token = jwt.sign(
             {email: fetchedUser.correo_electronico , userId : fetchedUser ._id } ,
             process.env.TOKEN_SECRET,
@@ -38,12 +41,25 @@ module.exports = {
               role: fetchedUser.roles,
               message: "Login exitoso."
             });
-        })
-        .catch(err =>{
-          return res.status(401).json({
-            message: "Autenticacion fallida."
-          });
+        }
+        codigo=generador(6, false, "^[0-9]+$");
+
+        const msg = {
+          to: fetchedUser.correo_electronico, 
+          from: 'k.zapata1@utp.edu.co', 
+          subject: 'Código de autenticación',
+          text: 'Su código de autenticación es ' + codigo,
+          html: '<strong>Su código de autenticación es ' + codigo + '</strong>',
+        }
+        await sgMail.send(msg);
+        fetchedUser.codigo_autenticacion=codigo;
+        await fetchedUser.save();
+        return res.status(200).json({});
+      } catch (e) {
+        return res.status(500).json({
+          error: e
         });
+      }
     },
     Signup: (req, res) => {
         clave = generador(8, false);
